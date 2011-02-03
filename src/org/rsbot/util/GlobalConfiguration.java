ae40bc60cb04185ec1d6a45c05c277d7e629f5ea
Liang <liang@powerbot.org>
2011-02-03 21:35:47 +0000

    v2.25
    git-svn-id: https://rsbot-client.googlecode.com/svn/trunk@70 128dfb44-bd8a-652a-bd9a-56b36ba659b7

--- src/org/rsbot/util/GlobalConfiguration.java
+++ src/org/rsbot/util/GlobalConfiguration.java
@@ -62,10 +62,10 @@ public class GlobalConfiguration {
 		}
 
 		public static class URLs {
-			public static final String UPDATER = "http://updater.powerbot.org/";
-			public static final String DOWNLOAD = UPDATER + "RSBot.jar";
-			public static final String UPDATE = UPDATER + "mod.dat";
-			public static final String VERSION = UPDATER + "version.dat";
+			public static final String UPDATER = "http://links.powerbot.org/";
+			public static final String DOWNLOAD = UPDATER + "update";
+			public static final String UPDATE = UPDATER + "modscript";
+			public static final String VERSION = UPDATER + "version";
 			public static final String PROJECT = "http://code.google.com/p/rsbot-client";
 			public static final String SITE = "http://www.powerbot.org";
 			public static final String STATS = "http://stats.powerbot.org/sync/";
