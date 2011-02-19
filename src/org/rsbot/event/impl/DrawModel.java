91bad72ecb610c77a339a141d113db470486f2e5
Pervy <pervy@powerbot.org>
2011-02-19 00:50:27 +0000

    Last update only pushed randoms
    Making sure all pushed
    
    git-svn-id: https://rsbot-client.googlecode.com/svn/trunk@73 128dfb44-bd8a-652a-bd9a-56b36ba659b7

--- src/org/rsbot/event/impl/DrawModel.java
+++ src/org/rsbot/event/impl/DrawModel.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.HashMap;
 
 import org.rsbot.bot.Bot;
@@ -15,7 +18,7 @@ import org.rsbot.script.wrappers.RSObject;
 
 //Draws a nice wire frame on interactable objects on screen.
 //Thanks to Kosaki for his initial idea and part of this code:)
-public class DrawModel implements PaintListener {
+public class DrawModel implements PaintListener, MouseListener {
 
 	private static final HashMap<RSObject.Type, Color> color_map = new HashMap<RSObject.Type, Color>();
 
@@ -26,6 +29,10 @@ public class DrawModel implements PaintListener {
 		color_map.put(RSObject.Type.WALL_DECORATION, Color.GRAY);
 	}
 
+	private final static String[] WORD_LIST = { "Objects", "Players", "NPCS",
+			"GroundItem" };
+	private static boolean[] Click = { true, true, true, true };
+
 	private MethodContext ctx;
 
 	public DrawModel(Bot bot) {
@@ -33,47 +40,99 @@ public class DrawModel implements PaintListener {
 	}
 
 	public void onRepaint(Graphics render) {
-		for (org.rsbot.script.wrappers.RSObject o : ctx.objects.getAll()) {
-			RSModel model = o.getModel();
-			if (model != null) {
-				render.setColor(color_map.get(o.getType()));
-				for (Polygon polygon : model.getTriangles()) {
-					render.drawPolygon(polygon);
+		DrawRect(render);
+		if (Click[0])
+			for (org.rsbot.script.wrappers.RSObject o : ctx.objects.getAll()) {
+				RSModel model = o.getModel();
+				if (model != null) {
+					render.setColor(color_map.get(o.getType()));
+					for (Polygon polygon : model.getTriangles()) {
+						render.drawPolygon(polygon);
+					}
+					render.setColor(Color.GREEN);
+					Point p = model.getPoint();
+					render.fillOval(p.x - 1, p.y - 1, 2, 2);
 				}
-				render.setColor(Color.GREEN);
-				Point p = model.getPoint();
-				render.fillOval(p.x - 1, p.y - 1, 2, 2);
 			}
-		}
+		if (Click[1])
+			for (org.rsbot.script.wrappers.RSCharacter c : ctx.players.getAll()) {
+				RSModel model = c.getModel();
+				if (model != null) {
+					render.setColor(Color.RED);
+					for (Polygon polygon : model.getTriangles()) {
+						render.drawPolygon(polygon);
+					}
+				}
+			}
+		if (Click[2])
+			for (org.rsbot.script.wrappers.RSCharacter c : ctx.npcs.getAll()) {
+				RSModel model = c.getModel();
+				if (model != null) {
+					render.setColor(Color.MAGENTA);
+					for (Polygon polygon : model.getTriangles()) {
+						render.drawPolygon(polygon);
+					}
+				}
+			}
+		if (Click[3])
+			for (RSGroundItem item : ctx.groundItems.getAll()) {
+				RSModel model = item.getModel();
+				if (model != null) {
+					render.setColor(Color.CYAN);
+					for (Polygon polygon : model.getTriangles()) {
+						render.drawPolygon(polygon);
+					}
+				}
+			}
+	}
 
-		for (org.rsbot.script.wrappers.RSCharacter c : ctx.players.getAll()) {
-			RSModel model = c.getModel();
-			if (model != null) {
-				render.setColor(Color.RED);
-				for (Polygon polygon : model.getTriangles()) {
-					render.drawPolygon(polygon);
-				}
-			}
+	public final void DrawRect(Graphics render) {
+		Color j = Color.BLACK;
+		Color w = Color.white;
+		for (int i = 0; i < WORD_LIST.length; i++) {
+			int alpha = 150;
+			render.setColor(new Color(j.getRed(), j.getGreen(), j.getBlue(),
+					alpha));
+			if (Click[i])
+				render.setColor(new Color(w.getRed(), w.getGreen(),
+						w.getBlue(), alpha));
+			render.fillRect(90 + (80 * i), 3, 80, 12);
+			render.setColor(Color.white);
+			if (Click[i])
+				render.setColor(Color.BLACK);
+			render.drawString(WORD_LIST[i], 90 + (80 * i) + 10, 13);
+			render.setColor(Color.black);
+			render.drawRect(90 + (80 * i), 3, 80, 12);
 		}
+	}
 
-		for (org.rsbot.script.wrappers.RSCharacter c : ctx.npcs.getAll()) {
-			RSModel model = c.getModel();
-			if (model != null) {
-				render.setColor(Color.MAGENTA);
-				for (Polygon polygon : model.getTriangles()) {
-					render.drawPolygon(polygon);
-				}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		for (int i = 0; i < WORD_LIST.length; i++) {
+			Rectangle rect = new Rectangle(90 + (80 * i), 3, 80, 12);
+			if (rect.contains(e.getPoint())) {
+				Click[i] = !Click[i];
 			}
 		}
-		
-		for(RSGroundItem item : ctx.groundItems.getAll()) {
-			RSModel model = item.getModel();
-			if(model != null) {
-				render.setColor(Color.CYAN);
-				for (Polygon polygon : model.getTriangles()) {
-					render.drawPolygon(polygon);
-				}
-			}
-		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+
 	}
 }
