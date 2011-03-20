2bcb53339ac22ef30552591a8c0b4f2148eff6cc
pervy@powerbot.org <pervy@powerbot.org@128dfb44-bd8a-652a-bd9a-56b36ba659b7>
2011-03-20 22:51:53 +0000

    Updated GlobalConfigurations.java to download temp ms data and CodeReader.java to read the new ms data correctly.
    git-svn-id: http://rsbot-client.googlecode.com/svn/trunk@108 128dfb44-bd8a-652a-bd9a-56b36ba659b7

--- src/org/rsbot/util/GlobalConfiguration.java
+++ src/org/rsbot/util/GlobalConfiguration.java
@@ -1,11 +1,7 @@
 package org.rsbot.util;
 
-import org.rsbot.log.LogFormatter;
-import org.rsbot.log.SystemConsoleHandler;
-import org.rsbot.log.TextAreaLogHandler;
-
-import javax.swing.filechooser.FileSystemView;
-import java.awt.*;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -24,6 +20,12 @@ import java.util.Properties;
 import java.util.logging.FileHandler;
 import java.util.logging.LogManager;
 
+import javax.swing.filechooser.FileSystemView;
+
+import org.rsbot.log.LogFormatter;
+import org.rsbot.log.SystemConsoleHandler;
+import org.rsbot.log.TextAreaLogHandler;
+
 public class GlobalConfiguration {
 
 	public enum OperatingSystem {
@@ -33,38 +35,61 @@ public class GlobalConfiguration {
 	public static class Paths {
 		public static class Resources {
 			public static final String ROOT = "resources";
-                        public static final String THEME = "theme.pb";
+			public static final String THEME = "theme.pb";
 			public static final String SCRIPTS = Paths.SCRIPTS_NAME_SRC + "/";
-			public static final String ROOT_IMG = "/" + Resources.ROOT + "/images";
+			public static final String ROOT_IMG = "/" + Resources.ROOT
+					+ "/images";
 			public static final String ICON = Resources.ROOT_IMG + "/icon.png";
-			public static final String ICON_DELETE = Resources.ROOT_IMG + "/delete.png";
-			public static final String ICON_PLAY = Resources.ROOT_IMG + "/control_play_blue.png";
-			public static final String ICON_PAUSE = Resources.ROOT_IMG + "/control_pause.png";
-			public static final String ICON_ADD = Resources.ROOT_IMG + "/add.png";
-			public static final String ICON_ADD_OVER = Resources.ROOT_IMG + "/add_over.png";
-			public static final String ICON_ADD_DOWN = Resources.ROOT_IMG + "/add_down.png";
-			public static final String ICON_HOME = Resources.ROOT_IMG + "/home.png";
-			public static final String ICON_BOT = Resources.ROOT_IMG + "/bot.png";
-			public static final String ICON_CLOSE = Resources.ROOT_IMG + "/close.png";
-			public static final String ICON_CLOSE_OVER = Resources.ROOT_IMG + "/close_over.png";
-			public static final String ICON_TICK = Resources.ROOT_IMG + "/tick.png";
-			public static final String ICON_MOUSE = Resources.ROOT_IMG + "/mouse.png";
-			public static final String ICON_KEYBOARD = Resources.ROOT_IMG + "/keyboard.png";
-			public static final String ICON_CONNECT = Resources.ROOT_IMG + "/connect.png";
-			public static final String ICON_DISCONNECT = Resources.ROOT_IMG + "/disconnect.png";
-			public static final String ICON_START = Resources.ROOT_IMG + "/control_play.png";
-			public static final String ICON_SCRIPT_BDL = Resources.ROOT_IMG + "/script_bdl.png";
-			public static final String ICON_SCRIPT_DRM = Resources.ROOT_IMG + "/script_drm.png";
-			public static final String ICON_SCRIPT_PRE = Resources.ROOT_IMG + "/script_pre.png";
-			public static final String ICON_SCRIPT_SRC = Resources.ROOT_IMG + "/script_src.png";
+			public static final String ICON_DELETE = Resources.ROOT_IMG
+					+ "/delete.png";
+			public static final String ICON_PLAY = Resources.ROOT_IMG
+					+ "/control_play_blue.png";
+			public static final String ICON_PAUSE = Resources.ROOT_IMG
+					+ "/control_pause.png";
+			public static final String ICON_ADD = Resources.ROOT_IMG
+					+ "/add.png";
+			public static final String ICON_ADD_OVER = Resources.ROOT_IMG
+					+ "/add_over.png";
+			public static final String ICON_ADD_DOWN = Resources.ROOT_IMG
+					+ "/add_down.png";
+			public static final String ICON_HOME = Resources.ROOT_IMG
+					+ "/home.png";
+			public static final String ICON_BOT = Resources.ROOT_IMG
+					+ "/bot.png";
+			public static final String ICON_CLOSE = Resources.ROOT_IMG
+					+ "/close.png";
+			public static final String ICON_CLOSE_OVER = Resources.ROOT_IMG
+					+ "/close_over.png";
+			public static final String ICON_TICK = Resources.ROOT_IMG
+					+ "/tick.png";
+			public static final String ICON_MOUSE = Resources.ROOT_IMG
+					+ "/mouse.png";
+			public static final String ICON_KEYBOARD = Resources.ROOT_IMG
+					+ "/keyboard.png";
+			public static final String ICON_CONNECT = Resources.ROOT_IMG
+					+ "/connect.png";
+			public static final String ICON_DISCONNECT = Resources.ROOT_IMG
+					+ "/disconnect.png";
+			public static final String ICON_START = Resources.ROOT_IMG
+					+ "/control_play.png";
+			public static final String ICON_SCRIPT_BDL = Resources.ROOT_IMG
+					+ "/script_bdl.png";
+			public static final String ICON_SCRIPT_DRM = Resources.ROOT_IMG
+					+ "/script_drm.png";
+			public static final String ICON_SCRIPT_PRE = Resources.ROOT_IMG
+					+ "/script_pre.png";
+			public static final String ICON_SCRIPT_SRC = Resources.ROOT_IMG
+					+ "/script_src.png";
 
-			public static final String VERSION = Resources.ROOT + "/version.dat";
+			public static final String VERSION = Resources.ROOT
+					+ "/version.dat";
 		}
 
 		public static class URLs {
 			public static final String UPDATER = "http://links.powerbot.org/";
 			public static final String DOWNLOAD = UPDATER + "update";
-			public static final String UPDATE = UPDATER + "modscript";
+			public static final String TEMPMOD = "http://aut0rprogramming.net/";
+			public static final String UPDATE = TEMPMOD + "modscript/ms641.dat";
 			public static final String VERSION = UPDATER + "version";
 			public static final String PROJECT = "http://code.google.com/p/rsbot-client";
 			public static final String SITE = "http://www.powerbot.org";
@@ -79,30 +104,53 @@ public class GlobalConfiguration {
 		public static final String COMPILE_SCRIPTS_SH = "compile-scripts.sh";
 		public static final String COMPILE_FIND_JDK = "FindJDK.bat";
 
-		public static final String ROOT_IMG = Paths.ROOT + File.separator + "images";
-		public static final String ICON = Paths.ROOT_IMG + File.separator + "icon.png";
-		public static final String ICON_DELETE = Paths.ROOT_IMG + File.separator + "delete.png";
-		public static final String ICON_PLAY = Paths.ROOT_IMG + File.separator + "control_play_blue.png";
-		public static final String ICON_PAUSE = Paths.ROOT_IMG + File.separator + "control_pause.png";
-		public static final String ICON_ADD = Paths.ROOT_IMG + File.separator + "add.png";
-		public static final String ICON_ADD_OVER = Paths.ROOT_IMG + File.separator + "add_over.png";
-		public static final String ICON_ADD_DOWN = Paths.ROOT_IMG + File.separator + "add_down.png";
-		public static final String ICON_HOME = Paths.ROOT_IMG + File.separator + "home.png";
-		public static final String ICON_BOT = Paths.ROOT_IMG + File.separator + "bot.png";
-		public static final String ICON_CLOSE = Paths.ROOT_IMG + File.separator + "close.png";
-		public static final String ICON_CLOSE_OVER = Paths.ROOT_IMG + File.separator + "close_over.png";
-		public static final String ICON_TICK = Paths.ROOT_IMG + File.separator + "tick.png";
-		public static final String ICON_MOUSE = Paths.ROOT_IMG + File.separator + "mouse.png";
-		public static final String ICON_KEYBOARD = Paths.ROOT_IMG + File.separator + "keyboard.png";
-		public static final String ICON_CONNECT = Paths.ROOT_IMG + File.separator + "connect.png";
-		public static final String ICON_DISCONNECT = Paths.ROOT_IMG + File.separator + "disconnect.png";
-		public static final String ICON_START = Paths.ROOT_IMG + File.separator + "control_play.png";
-		public static final String ICON_SCRIPT_BDL = Paths.ROOT_IMG + File.separator + "script_bdl.png";
-		public static final String ICON_SCRIPT_DRM = Paths.ROOT_IMG + File.separator + "script_drm.png";
-		public static final String ICON_SCRIPT_PRE = Paths.ROOT_IMG + File.separator + "script_pre.png";
-		public static final String ICON_SCRIPT_SRC = Paths.ROOT_IMG + File.separator + "script_src.png";
+		public static final String ROOT_IMG = Paths.ROOT + File.separator
+				+ "images";
+		public static final String ICON = Paths.ROOT_IMG + File.separator
+				+ "icon.png";
+		public static final String ICON_DELETE = Paths.ROOT_IMG
+				+ File.separator + "delete.png";
+		public static final String ICON_PLAY = Paths.ROOT_IMG + File.separator
+				+ "control_play_blue.png";
+		public static final String ICON_PAUSE = Paths.ROOT_IMG + File.separator
+				+ "control_pause.png";
+		public static final String ICON_ADD = Paths.ROOT_IMG + File.separator
+				+ "add.png";
+		public static final String ICON_ADD_OVER = Paths.ROOT_IMG
+				+ File.separator + "add_over.png";
+		public static final String ICON_ADD_DOWN = Paths.ROOT_IMG
+				+ File.separator + "add_down.png";
+		public static final String ICON_HOME = Paths.ROOT_IMG + File.separator
+				+ "home.png";
+		public static final String ICON_BOT = Paths.ROOT_IMG + File.separator
+				+ "bot.png";
+		public static final String ICON_CLOSE = Paths.ROOT_IMG + File.separator
+				+ "close.png";
+		public static final String ICON_CLOSE_OVER = Paths.ROOT_IMG
+				+ File.separator + "close_over.png";
+		public static final String ICON_TICK = Paths.ROOT_IMG + File.separator
+				+ "tick.png";
+		public static final String ICON_MOUSE = Paths.ROOT_IMG + File.separator
+				+ "mouse.png";
+		public static final String ICON_KEYBOARD = Paths.ROOT_IMG
+				+ File.separator + "keyboard.png";
+		public static final String ICON_CONNECT = Paths.ROOT_IMG
+				+ File.separator + "connect.png";
+		public static final String ICON_DISCONNECT = Paths.ROOT_IMG
+				+ File.separator + "disconnect.png";
+		public static final String ICON_START = Paths.ROOT_IMG + File.separator
+				+ "control_play.png";
+		public static final String ICON_SCRIPT_BDL = Paths.ROOT_IMG
+				+ File.separator + "script_bdl.png";
+		public static final String ICON_SCRIPT_DRM = Paths.ROOT_IMG
+				+ File.separator + "script_drm.png";
+		public static final String ICON_SCRIPT_PRE = Paths.ROOT_IMG
+				+ File.separator + "script_pre.png";
+		public static final String ICON_SCRIPT_SRC = Paths.ROOT_IMG
+				+ File.separator + "script_src.png";
 
-		public static final String VERSION = Paths.ROOT + File.separator + "version.dat";
+		public static final String VERSION = Paths.ROOT + File.separator
+				+ "version.dat";
 
 		public static final String SCRIPTS_NAME_SRC = "scripts";
 		public static final String SCRIPTS_NAME_OUT = "Scripts";
@@ -110,17 +158,24 @@ public class GlobalConfiguration {
 		public static String getAccountsFile() {
 			final String path;
 			if (GlobalConfiguration.getCurrentOperatingSystem() == OperatingSystem.WINDOWS) {
-				path = System.getenv("APPDATA") + File.separator + GlobalConfiguration.NAME + "_Accounts.ini";
+				path = System.getenv("APPDATA") + File.separator
+						+ GlobalConfiguration.NAME + "_Accounts.ini";
 			} else {
-				path = Paths.getUnixHome() + File.separator + "." + GlobalConfiguration.NAME_LOWERCASE + "acct";
+				path = Paths.getUnixHome() + File.separator + "."
+						+ GlobalConfiguration.NAME_LOWERCASE + "acct";
 			}
 			return path;
 		}
 
 		public static String getHomeDirectory() {
-			final String env = System.getenv(GlobalConfiguration.NAME.toUpperCase() + "_HOME");
+			final String env = System.getenv(GlobalConfiguration.NAME
+					.toUpperCase() + "_HOME");
 			if ((env == null) || env.isEmpty())
-				return (GlobalConfiguration.getCurrentOperatingSystem() == OperatingSystem.WINDOWS ? FileSystemView.getFileSystemView().getDefaultDirectory().getAbsolutePath() : Paths.getUnixHome()) + File.separator + GlobalConfiguration.NAME;
+				return (GlobalConfiguration.getCurrentOperatingSystem() == OperatingSystem.WINDOWS ? FileSystemView
+						.getFileSystemView().getDefaultDirectory()
+						.getAbsolutePath()
+						: Paths.getUnixHome())
+						+ File.separator + GlobalConfiguration.NAME;
 			else
 				return env;
 		}
@@ -140,7 +195,7 @@ public class GlobalConfiguration {
 		public static String getBootCache() {
 			return Paths.getSettingsDirectory() + File.separator + "boot.txt";
 		}
-		
+
 		public static String getUIDsFile() {
 			return Paths.getSettingsDirectory() + File.separator + "uid.txt";
 		}
@@ -150,7 +205,8 @@ public class GlobalConfiguration {
 		}
 
 		public static String getScriptsDirectory() {
-			return Paths.getHomeDirectory() + File.separator + Paths.SCRIPTS_NAME_OUT;
+			return Paths.getHomeDirectory() + File.separator
+					+ Paths.SCRIPTS_NAME_OUT;
 		}
 
 		public static String getScriptsSourcesDirectory() {
@@ -202,7 +258,8 @@ public class GlobalConfiguration {
 	public static boolean RUNNING_FROM_JAR = false;
 
 	static {
-		final URL resource = GlobalConfiguration.class.getClassLoader().getResource(Paths.Resources.VERSION);
+		final URL resource = GlobalConfiguration.class.getClassLoader()
+				.getResource(Paths.Resources.VERSION);
 		if (resource != null) {
 			GlobalConfiguration.RUNNING_FROM_JAR = true;
 		}
@@ -239,17 +296,22 @@ public class GlobalConfiguration {
 		Properties logging = new Properties();
 		String logFormatter = LogFormatter.class.getCanonicalName();
 		String fileHandler = FileHandler.class.getCanonicalName();
-		logging.setProperty("handlers", TextAreaLogHandler.class.getCanonicalName() + "," + fileHandler);
+		logging.setProperty("handlers",
+				TextAreaLogHandler.class.getCanonicalName() + "," + fileHandler);
 		logging.setProperty(".level", "INFO");
-		logging.setProperty(SystemConsoleHandler.class.getCanonicalName() + ".formatter", logFormatter);
+		logging.setProperty(SystemConsoleHandler.class.getCanonicalName()
+				+ ".formatter", logFormatter);
 		logging.setProperty(fileHandler + ".formatter", logFormatter);
-		logging.setProperty(TextAreaLogHandler.class.getCanonicalName() + ".formatter", logFormatter);
-		logging.setProperty(fileHandler + ".pattern", Paths.getLogsDirectory() + File.separator + "%u.%g.log");
+		logging.setProperty(TextAreaLogHandler.class.getCanonicalName()
+				+ ".formatter", logFormatter);
+		logging.setProperty(fileHandler + ".pattern", Paths.getLogsDirectory()
+				+ File.separator + "%u.%g.log");
 		logging.setProperty(fileHandler + ".count", "10");
 		final ByteArrayOutputStream logout = new ByteArrayOutputStream();
 		try {
 			logging.store(logout, "");
-			LogManager.getLogManager().readConfiguration(new ByteArrayInputStream(logout.toByteArray()));
+			LogManager.getLogManager().readConfiguration(
+					new ByteArrayInputStream(logout.toByteArray()));
 		} catch (final Exception ignored) {
 		}
 
@@ -272,7 +334,8 @@ public class GlobalConfiguration {
 						pathfile.delete();
 					}
 					pathfile.createNewFile();
-					Writer out = new BufferedWriter(new FileWriter(Paths.getPathCache()));
+					Writer out = new BufferedWriter(new FileWriter(
+							Paths.getPathCache()));
 					out.write(path);
 					out.close();
 				} catch (final Exception e) {
@@ -284,8 +347,12 @@ public class GlobalConfiguration {
 
 	public static Image getImage(String resource, String path) {
 		try {
-			return Toolkit.getDefaultToolkit().getImage(GlobalConfiguration.RUNNING_FROM_JAR ?
-					GlobalConfiguration.class.getResource(resource) : new File(path).toURI().toURL());
+			return Toolkit
+					.getDefaultToolkit()
+					.getImage(
+							GlobalConfiguration.RUNNING_FROM_JAR ? GlobalConfiguration.class
+									.getResource(resource) : new File(path)
+									.toURI().toURL());
 		} catch (Exception ignored) {
 		}
 		return null;
@@ -310,10 +377,13 @@ public class GlobalConfiguration {
 		return buf.toString();
 	}
 
-	public static URLConnection getURLConnection(final URL url, final String referer) throws IOException {
+	public static URLConnection getURLConnection(final URL url,
+			final String referer) throws IOException {
 		final URLConnection con = url.openConnection();
-		con.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-		con.addRequestProperty("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+		con.addRequestProperty("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+		con.addRequestProperty("Accept-Charset",
+				"ISO-8859-1,utf-8;q=0.7,*;q=0.7");
 		con.addRequestProperty("Accept-Encoding", "gzip,deflate");
 		con.addRequestProperty("Accept-Language", "en-us,en;q=0.5");
 		con.addRequestProperty("Connection", "keep-alive");
@@ -328,7 +398,10 @@ public class GlobalConfiguration {
 
 	public static int getVersion() {
 		try {
-			InputStream is = RUNNING_FROM_JAR ? GlobalConfiguration.class.getClassLoader().getResourceAsStream(Paths.Resources.VERSION) : new FileInputStream(Paths.VERSION);
+			InputStream is = RUNNING_FROM_JAR ? GlobalConfiguration.class
+					.getClassLoader().getResourceAsStream(
+							Paths.Resources.VERSION) : new FileInputStream(
+					Paths.VERSION);
 
 			int off = 0;
 			byte[] b = new byte[2];
