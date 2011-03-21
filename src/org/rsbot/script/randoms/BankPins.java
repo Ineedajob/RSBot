2065fa411b599f021c5256095ffee2124455e836
Paris <paris@powerbot.org>
2011-03-21 19:25:36 +0000

    Zenzie fixed BankPins random.
    git-svn-id: https://rsbot-client.googlecode.com/svn/trunk@112 128dfb44-bd8a-652a-bd9a-56b36ba659b7

--- src/org/rsbot/script/randoms/BankPins.java
+++ src/org/rsbot/script/randoms/BankPins.java
@@ -5,60 +5,31 @@ import org.rsbot.script.Random;
 import org.rsbot.script.ScriptManifest;
 import org.rsbot.script.wrappers.RSComponent;
 
-@ScriptManifest(authors = { "Holo", "Gnarly", "Salty_Fish", "Pervy Shuya" }, name = "BankPin", version = 2.3)
+@ScriptManifest(authors = { "Zenzie" }, name = "BankPin", version = 1.0)
 public class BankPins extends Random {
 
 	@Override
 	public boolean activateCondition() {
-		return interfaces.get(13).isValid()
-				|| interfaces.getComponent(14, 34).isValid();
-	}
-
-	public void enterCode(final String pin) {
-		if (!interfaces.get(13).isValid())
-			return;
-		final RSComponent[] children = interfaces.get(13).getComponents();
-		int state = 0;
-		for (int i = 1; i < 5; i++) {
-			if (children[i].containsText("?")) {
-				state++;
-			}
-		}
-		state = 4 - state;
-		if (!interfaces.get(759).isValid())
-			return;
-		final RSComponent[] bankPin = interfaces.get(759).getComponents();
-		for (int i = 0; i < bankPin.length; i++) {
-			if (bankPin[i].containsText(pin.substring(state, state + 1))) {
-				bankPin[i].doClick();
-				sleep(random(500, 1000));
-				break;
-			}
-		}
+		return interfaces.get(13).getComponent(26).containsText("Please enter your PIN");
 	}
 
 	@Override
 	public int loop() {
-		String pin = AccountManager.getPin(account.getName(), getClass());
-		if (interfaces.get(14).isValid()) {
-			if (interfaces.getComponent(14, 34).doClick())
-				return -1;
-		} else if (interfaces.get(13).isValid()) {
-			if ((pin == null) || (pin.length() != 4)) {
-				log.severe("You must add a bank pin to your account.");
-				stopScript(false);
-			}
-			if (interfaces.get(14).isValid() || !interfaces.get(13).isValid()) {
-				interfaces.get(14).getComponent(3).doClick();
-				return -1;
-			}
-			enterCode(pin);
-			if (interfaces.get(211).isValid()) {
-				interfaces.get(211).getComponent(3).doClick();
-			} else if (interfaces.get(217).isValid()) {
-				sleep(random(10500, 12000));
-			}
-		}
-		return 500;
+        char[] bankPin = AccountManager.getPin(account.getName(), this.getClass()).toCharArray();
+        if(bankPin == null || bankPin.length != 4) {
+            log("There is no pin entered for this account. Stopping script.");
+            stopScript(true);
+            return -1;
+        }
+        RSComponent[] bankComponents = interfaces.get(759).getComponents();
+        for(char ch : bankPin) {
+            for(RSComponent comp : bankComponents) {
+                if(comp.getText().equals(Character.toString(ch))) {
+                    comp.doClick();
+                    sleep(random(750, 2500));
+                }
+            }
+        }
+		return -1;
 	}
 }
