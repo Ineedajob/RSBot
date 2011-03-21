b0dad6fc21ff88e30bea5c8d58799ba3c43fa4cb
Paris <paris@powerbot.org>
2011-03-21 19:25:50 +0000

    Aut0r increased memory usage.
    git-svn-id: https://rsbot-client.googlecode.com/svn/trunk@113 128dfb44-bd8a-652a-bd9a-56b36ba659b7

--- src/org/rsbot/Boot.java
+++ src/org/rsbot/Boot.java
@@ -10,7 +10,7 @@ public class Boot {
         String location = Boot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         location = URLDecoder.decode(location, "UTF-8").replaceAll("\\\\", "/");
         String os = System.getProperty("os.name").toLowerCase();
-        String flags = "-Xmx512m -Dsun.java2d.d3d=false";
+        String flags = "-Xmx1024m -Dsun.java2d.d3d=false";
 
         if (os.contains("windows")) {
             Runtime.getRuntime().exec("javaw " + flags + " -classpath \"" +
