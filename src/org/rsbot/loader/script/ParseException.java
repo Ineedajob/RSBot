7951c0076680525d68022d0a2aab526eeb65b2bf
Aut0r <aut0r@powerbot.org>
2011-04-04 15:51:21 +0000

    Reverting fail updates in r118.  This is not a game update.
    git-svn-id: https://rsbot-client.googlecode.com/svn/trunk@119 128dfb44-bd8a-652a-bd9a-56b36ba659b7

--- src/org/rsbot/loader/script/ParseException.java
+++ src/org/rsbot/loader/script/ParseException.java
@@ -3,7 +3,6 @@ package org.rsbot.loader.script;
 /**
  * @author Jacmob
  */
-@SuppressWarnings("serial")
 public class ParseException extends Exception {
 
 	public ParseException(String message) {
--- src/org/rsbot/util/FrameUtil.java
+++ src/org/rsbot/util/FrameUtil.java
@@ -1,25 +1,25 @@
 package org.rsbot.util;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 /**
- * 
+ *
  * @author Nader Sleiman
  */
 public class FrameUtil {
 
-	public static boolean setTheme(JFrame f, String name) {
-		try {
+    public static boolean setTheme(JFrame f, String name) {
+        try {
 
-			JFrame.setDefaultLookAndFeelDecorated(true);
-			UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin."
-					+ name);
-			SwingUtilities.updateComponentTreeUI(f);
-		} catch (Exception e) {
-			return false;
-		}
-		return true;
-	}
+            f.setDefaultLookAndFeelDecorated(true);
+            UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin." + name);
+            SwingUtilities.updateComponentTreeUI(f);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
