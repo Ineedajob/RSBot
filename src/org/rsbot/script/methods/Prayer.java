c9c352c8586de2567fcc4738191a99559b5cb9af
Joe <Timer150@Gmail.com>
2011-04-13 19:25:53 -0400

    Prayer additions.

--- src/org/rsbot/script/methods/Prayer.java
+++ src/org/rsbot/script/methods/Prayer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 /**
  * Prayer related operations.
- *
+ * 
  * @author Aut0r, kiko
  */
 public class Prayer extends MethodProvider {
@@ -17,7 +17,7 @@ public class Prayer extends MethodProvider {
 
 	/**
 	 * Provides Prayer Book(s) Information.
-	 *
+	 * 
 	 * @author Aut0r
 	 */
 	public static enum Book {
@@ -26,12 +26,9 @@ public class Prayer extends MethodProvider {
 				3, 8), MYSTIC_WILL(4, 9), ROCK_SKIN(5, 10), SUPERHUMAN_STRENGTH(
 				6, 13), IMPROVED_REFLEXES(7, 16), RAPID_RESTORE(8, 19), RAPID_HEAL(
 				9, 22), PROTECT_ITEM(10, 25), HAWK_EYE(11, 26), MYSTIC_LORE(12,
-		                                                                    27), STEEL_SKIN(13, 28), ULTIMATE_STRENGTH(
-				14, 31), INCREDIBLE_REFLEXES(
+				27), STEEL_SKIN(13, 28), ULTIMATE_STRENGTH(14, 31), INCREDIBLE_REFLEXES(
 				15, 34), PROTECT_FROM_SUMMONING(16, 35), PROTECT_FROM_MAGIC(17,
-		                                                                    37), PROTECT_FROM_MISSILES(18,
-		                                                                                               40), PROTECT_FROM_MELEE(
-				19, 43), EAGLE_EYE(
+				37), PROTECT_FROM_MISSILES(18, 40), PROTECT_FROM_MELEE(19, 43), EAGLE_EYE(
 				20, 44), MYSTIC_MIGHT(21, 45), RETRIBUTION(22, 46), REDEMPTION(
 				23, 49), SMITE(24, 52), CHIVALRY(25, 60), RAPID_RENEWAL(26, 65), PIETY(
 				27, 70), RIGOUR(28, 74), AUGURY(29, 77),
@@ -65,7 +62,7 @@ public class Prayer extends MethodProvider {
 
 	/**
 	 * Checks if the player's prayer book is set to Ancient Curses.
-	 *
+	 * 
 	 * @return <tt>true</tt> if Curses are enabled; otherwise <tt>false</tt>.
 	 */
 	public boolean isCursing() {
@@ -74,13 +71,14 @@ public class Prayer extends MethodProvider {
 
 	/**
 	 * Returns true if designated prayer is turned on.
-	 *
-	 * @param prayer The prayer to check.
+	 * 
+	 * @param prayer
+	 *            The prayer to check.
 	 * @return <tt>true</tt> if enabled; otherwise <tt>false</tt>.
 	 */
 	public boolean isPrayerOn(Book prayer) {
 		RSComponent[] prayers = methods.interfaces.getComponent(271, 7)
-		                                          .getComponents();
+				.getComponents();
 		for (RSComponent c : prayers) {
 			if (c.getComponentIndex() == prayer.getIndex()
 					&& c.getBackgroundColor() != -1) {
@@ -93,26 +91,30 @@ public class Prayer extends MethodProvider {
 	/**
 	 * Returns true if the quick prayer interface has been used to activate
 	 * prayers.
-	 *
+	 * 
 	 * @return <tt>true</tt> if quick prayer is on; otherwise <tt>false</tt>.
 	 */
 	public boolean isQuickPrayerOn() {
 		return methods.interfaces.getComponent(Game.INTERFACE_PRAYER_ORB, 2)
-		                         .getBackgroundColor() == 782;
+				.getBackgroundColor() == 782;
 	}
 
 	/**
 	 * Activates/deactivates a prayer via interfaces.
-	 *
-	 * @param prayer   The prayer to activate.
-	 * @param activate <tt>true</tt> to activate; <tt>false</tt> to deactivate.
-	 * @return <tt>true</tt> if the interface was clicked; otherwise <tt>false</tt>.
+	 * 
+	 * @param prayer
+	 *            The prayer to activate.
+	 * @param activate
+	 *            <tt>true</tt> to activate; <tt>false</tt> to deactivate.
+	 * @return <tt>true</tt> if the interface was clicked; otherwise
+	 *         <tt>false</tt>.
 	 */
 	public boolean setPrayer(final Book prayer, final boolean activate) {
 		if (isPrayerOn(prayer) == activate) {
 			return false;
 		}
-		RSComponent pray = methods.interfaces.getComponent(271, 7).getComponent(prayer.getIndex());
+		RSComponent pray = methods.interfaces.getComponent(271, 7)
+				.getComponent(prayer.getIndex());
 		// Check that the background color == activate, otherwise click
 		return (pray.getBackgroundColor() == -1) == activate
 				&& pray.doAction(activate ? "Activate" : "Deactivate");
@@ -120,33 +122,57 @@ public class Prayer extends MethodProvider {
 
 	/**
 	 * Activates/deactivates quick prayers via interfaces.
-	 *
-	 * @param prayer   The prayer to activate.
-	 * @param activate <tt>true</tt> to activate; <tt>false</tt> to deactivate.
+	 * 
+	 * @param prayer
+	 *            The prayer to activate.
+	 * @param activate
+	 *            <tt>true</tt> to activate; <tt>false</tt> to deactivate.
 	 * @return <tt>true</tt> if the interface was clicked; otherwise
 	 *         <tt>false</tt>.
 	 */
 	public boolean setQuickPrayer(final boolean activate) {
-		return methods.interfaces.getComponent(749, 2).doAction(activate ? "on" : "off");
+		return methods.interfaces.getComponent(749, 2).doAction(
+				activate ? "on" : "off");
 	}
 
 	/**
 	 * Returns an array of RSComponents representing the prayers that are
 	 * selected.
-	 *
+	 * 
 	 * @return An <code>RSComponent</code> array containing all the components
 	 *         that represent selected prayers.
 	 */
 	public RSComponent[] getSelectedPrayers() {
 		ArrayList<RSComponent> selected = new ArrayList<RSComponent>();
 		RSComponent[] prayers = methods.interfaces.getComponent(271, 7)
-		                                          .getComponents();
+				.getComponents();
 		for (RSComponent prayer : prayers) {
 			if (prayer.getBackgroundColor() != -1) {
 				selected.add(prayer);
 			}
 		}
 		return selected.toArray(new RSComponent[selected.size()]);
+	}
+
+	/**
+	 * Gets the remaining prayer points.
+	 * 
+	 * @return The number of prayer points left.
+	 */
+	public int getPrayerLeft() {
+		return Integer.parseInt(methods.interfaces.getComponent(
+				methods.game.INTERFACE_PRAYER_ORB, 4).getText());
+	}
+
+	/**
+	 * Gets the percentage of prayer points left based on the players current
+	 * prayer level.
+	 * 
+	 * @return The percentage of prayer points left.
+	 */
+	public int getPrayerPercentLeft() {
+		return (100 * getPrayerLeft())
+				/ methods.skills.getCurrentLevel(Skills.PRAYER);
 	}
 
 }
