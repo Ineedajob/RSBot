47e1843e9bd2059ddac3a339bdcbe45ea29bcca7
Darren <dmjgilbert@googlemail.com>
2011-04-15 20:34:28 +0100

    Fix - Tabs     - Updated tonew clan chat (Still adding friends chat)     - Added clan vexillum to interface closer random

--- src/org/rsbot/script/randoms/CloseAllInterface.java
+++ src/org/rsbot/script/randoms/CloseAllInterface.java
@@ -13,91 +13,88 @@ import java.util.List;
 @ScriptManifest(authors = {"Jacmob", "HeyyamaN", "Pervy Shuya"}, name = "InterfaceCloser", version = 1.9)
 public class CloseAllInterface extends Random {
 
-	static class ComponentDef {
+    static class ComponentDef {
 
-		int parent;
-		int child;
-		boolean text;
+        int parent;
+        int child;
+        boolean text;
 
-		public ComponentDef(int parent, int child, boolean text) {
-			this.parent = parent;
-			this.child = child;
-			this.text = text;
-		}
+        public ComponentDef(int parent, int child, boolean text) {
+            this.parent = parent;
+            this.child = child;
+            this.text = text;
+        }
+    }
+    private final List<ComponentDef> components = new LinkedList<ComponentDef>();
 
-	}
+    {
+        addChild(743, 20); // Audio
+        addChild(767, 10); // Bank help
+        addChild(499, 29); // Stats
+        addChild(594, 48); // Report
+        addChild(275, 8); // Quest
+        addChild(206, 13); // Price check
+        addChild(266, 11); // Grove
+        addChild(102, 13); // Death items
+        addChild(14, 88, true); // New pin
+        addChild(14, 11); // Pin settings
+        addChild(157, 13); // Quick chat help
+        addChild(764, 2); // Objectives
+        addChild(895, 19); // Advisor
+        addChild(109, 14); // Grand exchange collection
+        addChild(667, 74); // Equipment Bonus
+        addChild(742, 14); // Graphic
+        addChild(917, 69); // Task List
+        addChild(1107, 174); // Clan Vexillum
+    }
 
-	private final List<ComponentDef> components = new LinkedList<ComponentDef>();
+    private void addChild(int parent, int idx) {
+        components.add(new ComponentDef(parent, idx, false));
+    }
 
-	{
-		addChild(743, 20); // Audio
-		addChild(767, 10); // Bank help
-		addChild(499, 29); // Stats
-		addChild(594, 48); // Report
-		addChild(275, 8); // Quest
-		addChild(206, 13); // Price check
-		addChild(266, 11); // Grove
-		addChild(102, 13); // Death items
-		addChild(14, 88, true); // New pin
-		addChild(14, 11); // Pin settings
-		addChild(157, 13); // Quick chat help
-		addChild(764, 2); // Objectives
-		addChild(895, 19); // Advisor
-		addChild(109, 14); // Grand exchange collection
-		addChild(667, 74); // Equipment Bonus
-		addChild(742, 14); // Graphic
-		addChild(917, 69); // Task List
-	}
+    private void addChild(int parent, int idx, boolean text) {
+        components.add(new ComponentDef(parent, idx, text));
+    }
 
-	private void addChild(int parent, int idx) {
-		components.add(new ComponentDef(parent, idx, false));
-	}
+    @Override
+    public boolean activateCondition() {
+        if (game.isLoggedIn()) {
+            if (interfaces.get(755).getComponent(44).isValid()) { // World map
+                if (interfaces.getComponent(755, 0).getComponents().length > 0) {
+                    return true;
+                }
+            }
+            for (ComponentDef c : components) {
+                RSComponent comp = interfaces.getComponent(c.parent, c.child);
+                if (comp.isValid()
+                        && !(c.text && (comp.getText() == null || comp.getText().isEmpty()))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-	private void addChild(int parent, int idx, boolean text) {
-		components.add(new ComponentDef(parent, idx, text));
-	}
+    @Override
+    public int loop() {
+        sleep(random(500, 900));
 
-	@Override
-	public boolean activateCondition() {
-		if (game.isLoggedIn()) {
-			if (interfaces.get(755).getComponent(44).isValid()) { // World map
-				if (interfaces.getComponent(755, 0).getComponents().length > 0) {
-					return true;
-				}
-			}
-			for (ComponentDef c : components) {
-				RSComponent comp = interfaces.getComponent(c.parent, c.child);
-				if (comp.isValid()
-						&& !(c.text && (comp.getText() == null || comp
-						.getText().isEmpty()))) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+        if (interfaces.get(755).isValid()
+                && (interfaces.getComponent(755, 0).getComponents().length > 0)) {
+            interfaces.getComponent(755, 44).doClick();
+            return random(500, 900);
+        }
+        for (ComponentDef c : components) {
+            if (interfaces.getComponent(c.parent, c.child).isValid()) {
+                interfaces.getComponent(c.parent, c.child).doClick();
+                sleep(random(500, 900));
+                if (random(0, 3) == 0) {
+                    mouse.moveSlightly();
+                }
+                break;
+            }
+        }
 
-	@Override
-	public int loop() {
-		sleep(random(500, 900));
-
-		if (interfaces.get(755).isValid()
-				&& (interfaces.getComponent(755, 0).getComponents().length > 0)) {
-			interfaces.getComponent(755, 44).doClick();
-			return random(500, 900);
-		}
-		for (ComponentDef c : components) {
-			if (interfaces.getComponent(c.parent, c.child).isValid()) {
-				interfaces.getComponent(c.parent, c.child).doClick();
-				sleep(random(500, 900));
-				if (random(0, 3) == 0) {
-					mouse.moveSlightly();
-				}
-				break;
-			}
-		}
-
-		return -1;
-	}
-
+        return -1;
+    }
 }
