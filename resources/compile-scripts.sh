424bdcbfe39de88ee44412c079009b1a28e9b25f
Ptolemaeus Lastname <sunderimehta@rediffmail.com>
2011-04-20 15:49:54 -0500

    syntax fixed, now working

--- resources/compile-scripts.sh
+++ resources/compile-scripts.sh
@@ -6,17 +6,28 @@ scripts=Scripts/Sources
 scriptspre=Scripts/Precompiled
 jarpathfile=Settings/path.txt
 
+cd ~/RSBot
+
 if [ ! -e "$jarpathfile" ]; then
 	echo "Path file does not exist. Please run RSBot and try again."
 	exit
 fi
 
-if [ ! -e $scripts/*.java ]; then
-	echo "No .java script source files found."
-	exit
-fi
-
+for file in $scripts/*.java
+do
+    if [ ! -e "${file}" ]; then
+    	echo "No .java script source files found."
+    	exit
+    fi
+done
 
 echo "Compiling scripts"
-if [ -e $scripts/*.class ]; then rm -R $scripts/*.class; fi
+for file in $scripts/*.class
+do
+    if [ -e "${file}" ]; then
+    	rm -R $scripts/*.class
+    	break
+    fi
+done
+
 "$cc" $cflags -cp "/$(cat $jarpathfile)" $scripts/*.java
