36da0ab583ec3b3f79d1178eb5278cb7cf56c782
Joe <Timer150@Gmail.com>
2011-04-26 15:21:56 -0400

    Reverted some bad commit.

--- src/org/rsbot/event/impl/DrawWeb.java
+++ src/org/rsbot/event/impl/DrawWeb.java
@@ -2,7 +2,6 @@ package org.rsbot.event.impl;
 
 import org.rsbot.bot.Bot;
 import org.rsbot.event.listeners.PaintListener;
-import org.rsbot.script.methods.Game;
 import org.rsbot.script.methods.MethodContext;
 import org.rsbot.script.wrappers.*;
 
@@ -20,9 +19,8 @@ public class DrawWeb implements PaintListener {
 		double minimapAngle = -1 * Math.toRadians(ctx.camera.getAngle());
 		int x = (tile.getX() - player.getLocation().getX()) * 4 - 2;
 		int y = (player.getLocation().getY() - tile.getY()) * 4 - 2;
-		return new Point((int) Math.round(x * Math.cos(minimapAngle) + y
-				* Math.sin(minimapAngle) + 628), (int) Math.round(y
-				* Math.cos(minimapAngle) - x * Math.sin(minimapAngle) + 87));
+		return new Point((int) Math.round(x * Math.cos(minimapAngle) + y * Math.sin(minimapAngle) + 628),
+				(int) Math.round(y * Math.cos(minimapAngle) - x * Math.sin(minimapAngle) + 87));
 	}
 
 	public void onRepaint(final Graphics render) {
@@ -37,21 +35,17 @@ public class DrawWeb implements PaintListener {
 		final WebMap map = new Web(ctx, null, null).map();
 		final WebTile[] webTiles = map.getTiles();
 		for (WebTile webTile : webTiles) {
-			if (ctx.calc.distanceTo(webTile.tile()) < 100
-					&& webTile.tile().getZ() == ctx.game.getPlane()) {
+			if (ctx.calc.distanceTo(webTile.tile()) < 100) {
 				Point p = tileToMap(webTile, player);
 				for (int l : webTile.connectingIndex()) {
-					if (webTiles[l].getZ() == ctx.game.getPlane()) {
-						Point pp = tileToMap(webTiles[l], player);
-						render.drawLine(pp.x, pp.y, p.x, p.y);
-					}
+					Point pp = tileToMap(webTiles[l], player);
+					render.drawLine(pp.x, pp.y, p.x, p.y);
 				}
 			}
 		}
 		render.setColor(Color.red);
 		for (WebTile webTile : map.getTiles()) {
-			if (ctx.calc.distanceTo(webTile.tile()) < 100
-					&& webTile.tile().getZ() == ctx.game.getPlane()) {
+			if (ctx.calc.distanceTo(webTile.tile()) < 100) {
 				Point p = tileToMap(webTile, player);
 				render.fillRect(p.x - 2, p.y - 2, 4, 4);
 			}
