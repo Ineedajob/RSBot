6eb91d440820828d90a2b569515549c9af9dfd45
Darren <dmjgilbert@googlemail.com>
2011-04-28 20:25:32 +0100

    Fixed modscript for npc levels. Using http://debaucheryscripts.host22.com/modscript.gz.

--- src/org/rsbot/event/impl/DrawNPCs.java
+++ src/org/rsbot/event/impl/DrawNPCs.java
@@ -41,7 +41,7 @@ public class DrawNPCs implements PaintListener {
 			// int x = element.getX();
 			// x -= ((int)(x >> 7)) << 7;
 			if (npc.getAnimation() != -1 || npc.getGraphic() != -1) {
-				s = "(A: " + npc.getAnimation() + " | G: " + npc.getGraphic() + ")";
+				s = "(A: " + npc.getAnimation() + " | G: " + npc.getGraphic() + " | L: " + npc.getLevel() +")";
 				render.drawString(s, location.x - metrics.stringWidth(s) / 2, location.y - metrics.getHeight() * 3 / 2);
 			}
 			// s = "" + element.isMoving();
