2190fd32345a9e75a56cd4d5399e7f343c0b0fd0
Paris <paris@powerbot.org>
2011-04-30 16:08:25 +0100

    Removed dead code.

--- src/org/rsbot/Application.java
+++ src/org/rsbot/Application.java
@@ -20,7 +20,6 @@ public class Application {
 		gui = new BotGUI();
 		gui.setVisible(true);
 		gui.addBot();
-		// stats();
 	}
 
 	/**
@@ -56,49 +55,4 @@ public class Application {
 		});
 		System.setErr(new PrintStream(new LogOutputStream(Logger.getLogger("STDERR"), Level.SEVERE), true));
 	}
-
-/*	private static void stats() {
-		sync(true);
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-
-			public void run() {
-				sync(false);
-			}
-		});
-	}
-
-	*//**
-	 * Notifies the project site of bot activity anonymously for
-	 * general statistics. Although this anonymous submission can
-	 * easily be abused, this will give some useful data over time.
-	 *
-	 * @param start <tt>true</tt> if sending startup message;
-	 *              <tt>false</tt> if sending shutdown message.
-	 *//*
-	private static void sync(boolean start) {
-		try {
-			URL url = new URL(GlobalConfiguration.Paths.URLs.STATS);
-			HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-			connect.setRequestMethod("GET");
-			connect.setDoOutput(true);
-			connect.setDoInput(true);
-			connect.setUseCaches(false);
-			connect.setAllowUserInteraction(false);
-			StringBuilder write = new StringBuilder("s=");
-			if (start) {
-				write.append("1");
-				startTime = System.currentTimeMillis();
-			} else {
-				write.append("0&t=").append((System.currentTimeMillis() - startTime) /
-						60000);
-			}
-			Writer writer = new OutputStreamWriter(connect.getOutputStream(),
-					"UTF-8");
-			writer.write(write.toString());
-			writer.flush();
-			writer.close();
-			connect.disconnect();
-		} catch (IOException ignored) {
-		}
-	}*/
 }
